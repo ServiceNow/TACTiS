f2322b75ce8b6caaf8873f5a051e0b046b059a71
Étienne Marcotte <etienne.marcotte@servicenow.com>
2022-05-30 21:24:16 +0000

    Signatures of TACTiS module and submodules

---
+++ tactis/model/tactis.py
@@ -0,0 +1,76 @@
+"""
+Copyright 2022 ServiceNow
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+>> The highest level model for TACTiS, which contains both its encoder and decoder.
+"""
+
+
+import torch
+from torch import nn
+
+
+class TACTiS(nn.Module):
+    """
+    The top-level module for TACTiS.
+
+    The role of this module is to handle everything outside of the encoder and decoder.
+    This consists mainly the data manipulation ahead of the encoder and after the decoder.
+    """
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, hist_time: torch.Tensor, hist_value: torch.Tensor, pred_time: torch.Tensor, pred_value: torch.Tensor) -> torch.Tensor:
+        """
+        Compute the loss function of the model.
+
+        Parameters:
+        -----------
+        hist_time: Tensor [batch, variables, time steps] or [batch, 1, time steps] or [batch, time steps]
+            A tensor containing the times associated with the values of hist_value.
+            If the time variable dimension is singleton or missing, then the time steps are taken as constant across all variables.
+        hist_value: Tensor [batch, variables, time steps]
+            A tensor containing the values that will be available at inference time.
+        pred_time: Tensor [batch, variables, time steps] or [batch, 1, time steps] or [batch, time steps]
+            A tensor containing the times associated with the values of pred_value.
+            If the time variable dimension is singleton or missing, then the time steps are taken as constant across all variables.
+        pred_value: Tensor [batch, variables, time steps]
+            A tensor containing the values that the model should learn to forecast at inference time.
+
+        Returns:
+        --------
+        loss: torch.Tensor [batch]
+            The loss function of TACTiS, with lower values being better.
+        """
+        pass
+    
+    def sample(self, num_samples: int, hist_time: torch.Tensor, hist_value: torch.Tensor, pred_time: torch.Tensor) -> torch.Tensor:
+        """
+        Generate the given number of samples from the forecasted distribution.
+
+        Parameters:
+        -----------
+        num_samples: int
+            How many samples to generate, must be >= 1.
+        hist_time: Tensor [batch, variables, time steps] or [batch, 1, time steps] or [batch, time steps]
+            A tensor containing the times associated with the values of hist_value.
+            If the time variable dimension is singleton or missing, then the time steps are taken as constant across all variables.
+        hist_value: Tensor [batch, variables, time steps]
+            A tensor containing the available values
+        pred_time: Tensor [batch, variables, time steps] or [batch, 1, time steps] or [batch, time steps]
+            A tensor containing the times at which we want forecasts.
+            If the time variable dimension is singleton or missing, then the time steps are taken as constant across all variables.
+
+        Returns:
+        --------
+        samples: torch.Tensor [batch, variables, time steps, samples]
+            Samples from the forecasted distribution.
+        """
