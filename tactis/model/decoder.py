fbfa54533a1a2e10ee705246826692e0593d647e
Étienne Marcotte <etienne.marcotte@servicenow.com>
2022-05-30 21:24:16 +0000

    Signatures of TACTiS module and submodules

---
+++ tactis/model/decoder.py
@@ -0,0 +1,252 @@
+"""
+Copyright 2022 ServiceNow
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+>> The various decoders for TACTiS, to output 
+"""
+
+
+import torch
+from torch import nn
+
+
+class CopulaDecoder(nn.Module):
+    """
+    A decoder which forecast using a distribution built from a copula and marginal distributions.
+    """
+    def __init__(self):
+        super().__init__()
+
+    def loss(self, encoded: torch.Tensor, mask: torch.BoolTensor, true_value: torch.Tensor) -> torch.Tensor:
+        """
+        Compute the loss function of the decoder.
+
+        Parameters:
+        -----------
+        encoded: Tensor [batch, variables, time steps, embedding dimension]
+            A tensor containing an embedding for each variable and time step.
+            This embedding is coming from the encoder, so contains shared information across variables and time steps.
+        mask: BoolTensor [batch, variables, time steps]
+            A tensor containing a mask indicating whether a given value was available for the encoder.
+            The decoder only forecasts values for which the mask is set to False.
+        true_value: Tensor [batch, variables, time steps]
+            A tensor containing the true value for the values to be forecasted.
+            Only the values where the mask is set to False will be considered in the loss function. 
+
+        Returns:
+        --------
+        embedding: torch.Tensor [batch]
+            The loss function, equal to the negative log likelihood of the distribution.
+        """
+        pass
+
+    def sample(self, num_samples: int, encoded: torch.Tensor, mask: torch.BoolTensor, true_value: torch.Tensor) -> torch.Tensor:
+        """
+        Generate the given number of samples from the forecasted distribution.
+
+        Parameters:
+        -----------
+        num_samples: int
+            How many samples to generate, must be >= 1.
+        encoded: Tensor [batch, variables, time steps, embedding dimension]
+            A tensor containing an embedding for each variable and time step.
+            This embedding is coming from the encoder, so contains shared information across variables and time steps.
+        mask: BoolTensor [batch, variables, time steps]
+            A tensor containing a mask indicating whether a given value was available for the encoder.
+            The decoder only forecasts values for which the mask is set to False.
+        true_value: Tensor [batch, variables, time steps]
+            A tensor containing the true value for the values to be forecasted.
+            The values where the mask is set to True will be copied as-is in the output.
+
+        Returns:
+        --------
+        samples: torch.Tensor [batch, variables, time steps, samples]
+            Samples drawn from the forecasted distribution.
+        """
+        pass
+
+
+class AttentionalCopula(nn.Module):
+    """
+    A non-parametric copula based on attention between the various variables.
+    """
+    def __init__(self):
+        super().__init__()
+
+    def loss(self, hist_encoded: torch.Tensor, hist_true_u: torch.Tensor, pred_encoded: torch.Tensor, pred_true_u: torch.Tensor) -> torch.Tensor:
+        """
+        Compute the loss function of the copula portion of the decoder.
+
+        Parameters:
+        -----------
+        hist_encoded: Tensor [batch, variables * time steps, embedding dimension]
+            A tensor containing an embedding for each variable and time step that does not have to be forecasted.
+            The variables and time steps dimensions are merged.
+        hist_true_u: Tensor [batch, variables * time steps]
+            A tensor containing the true value for the values that do not have to be forecasted, transformed by the marginal distribution into U(0,1) values.
+            The variables and time steps dimensions are merged.
+        pred_encoded: Tensor [batch, variables * time steps, embedding dimension]
+            A tensor containing an embedding for each variable and time step that does have to be forecasted.
+            The variables and time steps dimensions are merged.
+        pred_true_u: Tensor [batch, variables * time steps]
+            A tensor containing the true value for the values to be forecasted, transformed by the marginal distribution into U(0,1) values.
+            The variables and time steps dimensions are merged.
+
+        Returns:
+        --------
+        embedding: torch.Tensor [batch]
+            The loss function, equal to the negative log likelihood of the copula.
+        """
+        pass
+
+    def sample(self, num_samples: int, hist_encoded: torch.Tensor, hist_true_u: torch.Tensor, pred_encoded: torch.Tensor) -> torch.Tensor:
+        """
+        Generate the given number of samples from the forecasted copula.
+
+        Parameters:
+        -----------
+        num_samples: int
+            How many samples to generate, must be >= 1.
+        hist_encoded: Tensor [batch, variables * time steps, embedding dimension]
+            A tensor containing an embedding for each variable and time step that does not have to be forecasted.
+            The variables and time steps dimensions are merged.
+        hist_true_u: Tensor [batch, variables * time steps]
+            A tensor containing the true value for the values that do not have to be forecasted, transformed by the marginal distribution into U(0,1) values.
+            The variables and time steps dimensions are merged.
+        pred_encoded: Tensor [batch, variables * time steps, embedding dimension]
+            A tensor containing an embedding for each variable and time step that does have to be forecasted.
+            The variables and time steps dimensions are merged.
+
+        Returns:
+        --------
+        samples: torch.Tensor [batch, variables * time steps, samples]
+            Samples drawn from the forecasted copula, thus in the [0, 1] range.
+            The variables and time steps dimensions are merged.
+        """
+        pass
+
+
+class TrivialCopula(nn.Module):
+    """
+    The trivial copula where all variables are independent.
+    """
+    def __init__(self):
+        super().__init__()
+
+    def loss(self, hist_encoded: torch.Tensor, hist_true_u: torch.Tensor, pred_encoded: torch.Tensor, pred_true_u: torch.Tensor) -> torch.Tensor:
+        """
+        Compute the loss function of the copula portion of the decoder.
+
+        Parameters:
+        -----------
+        hist_encoded: Tensor [batch, variables * time steps, embedding dimension]
+            A tensor containing an embedding for each variable and time step that does not have to be forecasted.
+            The variables and time steps dimensions are merged.
+        hist_true_u: Tensor [batch, variables * time steps]
+            A tensor containing the true value for the values that do not have to be forecasted, transformed by the marginal distribution into U(0,1) values.
+            The variables and time steps dimensions are merged.
+        pred_encoded: Tensor [batch, variables * time steps, embedding dimension]
+            A tensor containing an embedding for each variable and time step that does have to be forecasted.
+            The variables and time steps dimensions are merged.
+        pred_true_u: Tensor [batch, variables * time steps]
+            A tensor containing the true value for the values to be forecasted, transformed by the marginal distribution into U(0,1) values.
+            The variables and time steps dimensions are merged.
+
+        Returns:
+        --------
+        embedding: torch.Tensor [batch]
+            The loss function, equal to the negative log likelihood of the copula.
+            This is always equal to zero.
+        """
+        pass
+
+    def sample(self, num_samples: int, hist_encoded: torch.Tensor, hist_true_u: torch.Tensor, pred_encoded: torch.Tensor) -> torch.Tensor:
+        """
+        Generate the given number of samples from the trivial copula.
+
+        Parameters:
+        -----------
+        num_samples: int
+            How many samples to generate, must be >= 1.
+        hist_encoded: Tensor [batch, variables * time steps, embedding dimension]
+            A tensor containing an embedding for each variable and time step that does not have to be forecasted.
+            The variables and time steps dimensions are merged.
+        hist_true_u: Tensor [batch, variables * time steps]
+            A tensor containing the true value for the values that do not have to be forecasted, transformed by the marginal distribution into U(0,1) values.
+            The variables and time steps dimensions are merged.
+        pred_encoded: Tensor [batch, variables * time steps, embedding dimension]
+            A tensor containing an embedding for each variable and time step that does have to be forecasted.
+            The variables and time steps dimensions are merged.
+
+        Returns:
+        --------
+        samples: torch.Tensor [batch, variables * time steps, samples]
+            Samples drawn from the trivial copula, which is equal to the multi-dimensional Uniform(0, 1) distribution.
+            The variables and time steps dimensions are merged.
+        """
+        pass
+
+
+class GaussianDecoder(nn.Module):
+    """
+    A decoder which forecast using a low-rank multivariate Gaussian distribution.
+    """
+    def __init__(self):
+        super().__init__()
+
+    def loss(self, encoded: torch.Tensor, mask: torch.BoolTensor, true_value: torch.Tensor) -> torch.Tensor:
+        """
+        Compute the loss function of the decoder.
+
+        Parameters:
+        -----------
+        encoded: Tensor [batch, variables, time steps, embedding dimension]
+            A tensor containing an embedding for each variable and time step.
+            This embedding is coming from the encoder, so contains shared information across variables and time steps.
+        mask: BoolTensor [batch, variables, time steps]
+            A tensor containing a mask indicating whether a given value was available for the encoder.
+            The decoder only forecasts values for which the mask is set to False.
+        true_value: Tensor [batch, variables, time steps]
+            A tensor containing the true value for the values to be forecasted.
+            Only the values where the mask is set to False will be considered in the loss function. 
+
+        Returns:
+        --------
+        embedding: torch.Tensor [batch]
+            The loss function, equal to the negative log likelihood of the distribution.
+        """
+        pass
+
+    def sample(self, num_samples: int, encoded: torch.Tensor, mask: torch.BoolTensor, true_value: torch.Tensor) -> torch.Tensor:
+        """
+        Generate the given number of samples from the forecasted distribution.
+
+        Parameters:
+        -----------
+        num_samples: int
+            How many samples to generate, must be >= 1.
+        encoded: Tensor [batch, variables, time steps, embedding dimension]
+            A tensor containing an embedding for each variable and time step.
+            This embedding is coming from the encoder, so contains shared information across variables and time steps.
+        mask: BoolTensor [batch, variables, time steps]
+            A tensor containing a mask indicating whether a given value was available for the encoder.
+            The decoder only forecasts values for which the mask is set to False.
+        true_value: Tensor [batch, variables, time steps]
+            A tensor containing the true value for the values to be forecasted.
+            The values where the mask is set to True will be copied as-is in the output.
+
+        Returns:
+        --------
+        samples: torch.Tensor [batch, variables, time steps, samples]
+            Samples drawn from the forecasted distribution.
+        """
+        pass
+
---
+++ tactis/model/encoder.py
@@ -0,0 +1,89 @@
+"""
+Copyright 2022 ServiceNow
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+>> The two possible encoders for TACTiS, based on the Transformer architecture.
+"""
+
+
+import torch
+from torch import nn
+
+
+class Encoder(nn.Module):
+    """
+    The traditional encoder for TACTiS, based on the Transformer architecture.
+
+    The encoder receives an input which contains for each variable and time step:
+    * The variable value at the time step, masked to zero if part of the values to be forecasted
+    * The mask
+    * The embedding for the variable
+    * The embedding for the time step
+    And has already been through any input encoder.
+
+    The decoder returns an output containing an embedding for each variable and time step.
+    """
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, data: torch.Tensor) -> torch.Tensor:
+        """
+        Compute the embedding for each variable and time step.
+
+        Parameters:
+        -----------
+        data: Tensor [batch, variables, time steps, input embedding dimension]
+            A tensor containing an embedding for each variable and time step.
+            This embedding is expected to only contain local information, with no interaction between variables or time steps.
+
+        Returns:
+        --------
+        encoded: torch.Tensor [batch, variables, time steps, output embedding dimension]
+            The encoded embedding for each variable and time step.
+        """
+        pass
+
+
+class TemporalEncoder(nn.Module):
+    """
+    The encoder for TACTiS, based on the Temporal Transformer architecture.
+    This encoder alternate between doing self-attention between different variables of the same time steps,
+    and doing self-attention between different time steps of the same variables.
+    This greatly reduces the memory footprint compared to TACTiSEncoder.
+
+    The encoder receives an input which contains for each variable and time step:
+    * The variable value at the time step, masked to zero if part of the values to be forecasted
+    * The mask
+    * The embedding for the variable
+    * The embedding for the time step
+    And has already been through any input encoder.
+
+    The decoder returns an output containing an embedding for each variable and time step.
+    """
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, data: torch.Tensor) -> torch.Tensor:
+        """
+        Compute the embedding for each variable and time step.
+
+        Parameters:
+        -----------
+        data: Tensor [batch, variables, time steps, input embedding dimension]
+            A tensor containing an embedding for each variable and time step.
+            This embedding is expected to only contain local information, with no interaction between variables or time steps.
+
+        Returns:
+        --------
+        encoded: torch.Tensor [batch, variables, time steps, output embedding dimension]
+            The encoded embedding for each variable and time step.
+        """
+        pass
