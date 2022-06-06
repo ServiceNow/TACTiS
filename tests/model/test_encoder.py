308c677590900c4e3db6b39bbae50eca88ade068
Étienne Marcotte <etienne.marcotte@servicenow.com>
2022-06-06 18:59:35 +0000

    Implement the temporal encoder

--- tests/model/test_encoder.py
+++ tests/model/test_encoder.py
@@ -14,7 +14,7 @@ limitations under the License.
 import torch
 from torch import nn
 
-from tactis.model.encoder import Encoder
+from tactis.model.encoder import Encoder, TemporalEncoder
 
 
 def test_encoder():
@@ -29,6 +29,32 @@ def test_encoder():
     attention_dim = 7
 
     net = Encoder(
+        attention_layers=3,
+        attention_heads=attention_heads,
+        attention_dim=attention_dim,
+        attention_feedforward_dim=11,
+    )
+
+    assert net.embedding_dim == attention_heads * attention_dim
+
+    input = torch.rand(num_batches, num_series, num_timesteps, net.embedding_dim)
+    output = net.forward(input)
+
+    assert output.shape == (num_batches, num_series, num_timesteps, net.embedding_dim)
+
+
+def test_temporal_encoder():
+    """
+    Test that the TemporalEncoder class runs without error, with the correct dimensions.
+    """
+    num_batches = 4
+    num_series = 5
+    num_timesteps = 6
+
+    attention_heads = 2
+    attention_dim = 7
+
+    net = TemporalEncoder(
         attention_layers=3,
         attention_heads=attention_heads,
         attention_dim=attention_dim,
