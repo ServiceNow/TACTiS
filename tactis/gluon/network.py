5fbb5f07573f28594b380f7be3592cd0239d199e
Étienne Marcotte <etienne.marcotte@servicenow.com>
2022-06-30 15:36:09 +0000

    Fix errors in computing metrics (Gluon interface)

--- tactis/gluon/network.py
+++ tactis/gluon/network.py
@@ -55,6 +55,11 @@ class TACTiSTrainingNetwork(nn.Module):
             The historical data that will be available at inference time.
         future_target_norm: torch.Tensor [batch, time steps, series]
             The data to be forecasted at inference time.
+
+        Returns:
+        --------
+        loss: torch.Tensor []
+            The loss function, averaged over all batches.
         """
         # The data coming from Gluon is not in the shape we use in the model, so transpose it.
         hist_value = past_target_norm.transpose(1, 2)
@@ -100,6 +105,32 @@ class TACTiSPredictionNetwork(nn.Module):
     def forward(
         self,
         past_target_norm: torch.Tensor,
-        future_target_norm: torch.Tensor,
     ) -> torch.Tensor:
-        breakpoint()
+        """
+        Parameters:
+        -----------
+        past_target_norm: torch.Tensor [batch, time steps, series]
+            The historical data that are available.
+
+        Returns:
+        --------
+        samples: torch.Tensor [samples, batch, time steps, series]
+            Samples from the forecasted distribution.
+        """
+        # The data coming from Gluon is not in the shape we use in the model, so transpose it.
+        hist_value = past_target_norm.transpose(1, 2)
+
+        # For the time steps, we take for granted that the data is aligned with a constant frequency
+        hist_time = torch.arange(0, hist_value.shape[2], dtype=int, device=hist_value.device)[None, :].expand(
+            hist_value.shape[0], -1
+        )
+        pred_time = torch.arange(
+            hist_value.shape[2], hist_value.shape[2] + self.prediction_length, dtype=int, device=hist_value.device
+        )[None, :].expand(hist_value.shape[0], -1)
+
+        samples = self.model.sample(
+            num_samples=self.num_parallel_samples, hist_time=hist_time, hist_value=hist_value, pred_time=pred_time
+        )
+        # The model decoder returns both the observed and sampled values, so removed the observed ones.
+        # Also, reorder from [batch, series, time steps, samples] to GluonTS expected [batch, samples, time steps, series].
+        return samples[:, :, -self.prediction_length :, :].permute((0, 3, 2, 1))
