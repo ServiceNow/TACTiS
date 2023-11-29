9a8edc93df82d81b8ef1ff99bb52d46f743d4e1e
Arjun Ashok <arjun.ashok@servicenow.com>
2023-11-28 20:31:19 -0500

    Edit paths from notebooks (#28)

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def read_requirements(path):
 
 setup(
     name="tactis",
-    version="0.0.6",
+    version="0.0.9",
     description="Transformer-Attentional Copulas for Multivariate Time Series",
     url="https://github.com/ServiceNow/tactis/tree/tactis-2",
     long_description=read("README.md"),
